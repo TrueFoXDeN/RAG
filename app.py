122517d9914a131ba6fd5c89a344f631deb1249a
laurenz <laurenz.dilba@hotmail.de>
2024-09-14 19:23:27 +0200

    added embed and minio

--- app.py
+++ app.py
@@ -1,36 +1,53 @@
+from typing import List
+
 from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams
 from qdrant_client.models import PointStruct
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from starlette.responses import JSONResponse
+from sentence_transformers import SentenceTransformer
+from pydantic import BaseModel
+
+
+class EmbedRequest(BaseModel):
+    text: List[str]
+
 
 client = QdrantClient(url="http://localhost:6333")
 
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled": True})
+model = SentenceTransformer('all-MiniLM-L6-v2')
 
-def main():
+
+@app.get("/vector/setup")
+def setup_db():
     client.create_collection(
-        collection_name="test_collection",
-        vectors_config=VectorParams(size=4, distance=Distance.COSINE),
+        collection_name="embeddings",
+        vectors_config=VectorParams(size=384, distance=Distance.COSINE),
     )
-
-    operation_info = client.upsert(
-        collection_name="test_collection",
-        wait=True,
-        points=[
-            PointStruct(id=1, vector=[0.05, 0.61, 0.76, 0.74], payload={"city": "Berlin"}),
-            PointStruct(id=2, vector=[0.19, 0.81, 0.75, 0.11], payload={"city": "London"}),
-            PointStruct(id=3, vector=[0.36, 0.55, 0.47, 0.94], payload={"city": "Moscow"}),
-            PointStruct(id=4, vector=[0.18, 0.01, 0.85, 0.80], payload={"city": "New York"}),
-            PointStruct(id=5, vector=[0.24, 0.18, 0.22, 0.44], payload={"city": "Beijing"}),
-            PointStruct(id=6, vector=[0.35, 0.08, 0.11, 0.44], payload={"city": "Mumbai"}),
-        ],
-    )
+    return JSONResponse({"success": True})
 
 
 def search():
     search_result = client.query_points(
-        collection_name="test_collection", query=[0.2, 0.1, 0.9, 0.7], limit=3
+        collection_name="embeddings", query=[0.2, 0.1, 0.9, 0.7], limit=3
     ).points
-    print(search_result)
+    return search_result
 
 
-if __name__ == '__main__':
-    search()
+@app.get("/vector/search")
+async def vector_search():
+    return JSONResponse(content=jsonable_encoder(search()))
+
+
+@app.post("/vector/embed")
+async def vector_embed(embed: EmbedRequest):
+    vector = model.encode(embed.text)
+    print(vector)
+    vector_list = vector.tolist()  # Convert NumPy array to list
+    return vector_list  # FastAPI can handle serialization
+
+#
+# if __name__ == '__main__':
+#     search()
